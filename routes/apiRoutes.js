4545782162cbeb18e8549cd41e54d846fb933d10
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-09-06 03:12:18 -0400

    i lied, now the api get route is working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
+// const uuidv1 = require('uuid/v1');
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-    res.json(data);
+    const info = fs.readFileSync(path.join(__dirname, "../db/db.json"));
+    res.json(JSON.parse(info));
 });
 
+// router.post('/notes', (req, res) => {
+//     req.body.id = uuid.v4();
+
+// })
 
 
   module.exports = router;
