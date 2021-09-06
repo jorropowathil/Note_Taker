eab613c112afbf0cc725655f02103433dc9eec75
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-09-06 03:20:34 -0400

    added delete note route:

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,14 +4,19 @@ const path = require('path');
 // const uuidv1 = require('uuid/v1');
 
 router.get('/notes', (req, res) => {
-    const info = fs.readFileSync(path.join(__dirname, "../db/db.json"));
-    res.json(JSON.parse(info));
+    const info = fs.JSON.parse(readFileSync(path.join(__dirname, "../db/db.json")));
+    res.json(info);
 });
 
-// router.post('/notes', (req, res) => {
-//     req.body.id = uuid.v4();
+router.post('/notes', (req, res) => {
+    const data = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
+    // req.body.id = uuid.v4();
+    // console.log(req.body.id);
+})
 
-// })
+router.delete('/notes', (req, res) => {
+
+})
 
 
   module.exports = router;
